4e66c9f51466b7c74938a9490365dcc4516b2501
Maria Lipina <m.o.lip714@gmail.com>
2022-02-26 19:27:28 +0300

    убраны комментарии

--- converter.py
+++ converter.py
@@ -1,12 +1,11 @@
-from cmath import nan
 from fractions import Fraction
 
 def conversion(x):
-    try: return int(x) #8
+    try: return int(x)
     except: pass
-    try: return float(x) #8.1 или 1., 1.0, .1, 0.1, 1e+1, 1.e-3, 1.0e0
+    try: return float(x)
     except: pass
-    try: return Fraction(x) #8/3
+    try: return Fraction(x)
     except: pass
-    try: return complex(x) #8+3j или 8-3j
-    except: return nan
+    try: return complex(x)
+    except: return 'nan'
