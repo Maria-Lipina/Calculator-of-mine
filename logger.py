a0c30f69256b2d6a06a03f42e59fcf0b95537016
Maria Lipina <m.o.lip714@gmail.com>
2022-02-26 20:20:55 +0300

    словарь не подойдет

--- logger.py
+++ logger.py
@@ -1,25 +1,10 @@
-from random import randint
+import ui
+from datetime import datetime as dt
 
+history = {}
 
-test = {
-    1: 'q',
-    2: 'w',
-    3: 'e',
-    4: 'r',
-    5: 't'
-}
-
-rec = list(test.keys())
-print(rec)
-del test[rec[0]]
-record = {(rec[-1]+1): 'y'}
-test = test | record
-print(test)
-
-for i in range(10):
-    rec = list(test.keys())
-    del test[rec[0]]
-    record = {(rec[-1]+1): randint(0, 100)}
-    test = test | record
-    print(test)
-    print('---------')
+def log_it(record):
+    global history
+    new_rec = {dt.now().strftime('%d/%m/%Y %H:%M'): record}
+    history = new_rec | history
+    print(history)
