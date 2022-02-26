1745a0361b457a3677cfeaa59162c6d66995c49f
Maria Lipina <m.o.lip714@gmail.com>
2022-02-26 19:29:06 +0300

    методы для вычислений и команда вывода на экран

--- controller.py
+++ controller.py
@@ -1,11 +1,29 @@
 import ui
+import converter as con
+import calc
+
+def get_number():
+    num = con.conversion(ui.get_value())
+    if num == 'nan':
+        get_number()
+    else: return num
+
+def get_operation():
+    op = ui.get_value()
+    if op in ['+', '-', '*', '/']:
+        return op
+    else: get_operation()
 
 def button_click():
-    ui.hello()
     command = ui.menu()
     if command == '1':
-        print("here is instruction")
+        ui.instruct()
     if command == '2':
-        print("here is input of number-op-number --> calculator")
+        number1 = get_number()
+        operate = get_operation()
+        number2 = get_number()
+        result = calc.calculate(number1, operate, number2)
+        ui.show_result(number1, operate, number2, result)
     if command == '3':
         print("here is log of last 10 operations")
+    # else: button_click()
