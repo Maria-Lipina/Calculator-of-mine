1e732807599a7157fbc7b5fd29c281905923ceb9
Maria Lipina <m.o.lip714@gmail.com>
2022-02-26 22:04:29 +0300

    убрано лишнее

--- controller.py
+++ controller.py
@@ -16,15 +16,8 @@ def get_operation():
     else: get_operation()
 
 def button_click():
-    command = ui.menu()
-    if command == '1':
-        ui.instruct()
-    if command == '2':
-        number1 = get_number()
-        operate = get_operation()
-        number2 = get_number()
-        result = calc.calculate(number1, operate, number2)
-        l.log_it(ui.show_result(number1, operate, number2, result))
-    if command == '3':
-        print("here is log of last 10 operations")
-    # else: button_click()
+    number1 = get_number()
+    operate = get_operation()
+    number2 = get_number()
+    result = calc.calculate(number1, operate, number2)
+    l.log_it(ui.show_result(number1, operate, number2, result))
